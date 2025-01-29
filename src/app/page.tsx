df003805b3fe97c741dd7e553cee5309e57220e3
SempaiDarcy <mr_darcy_1@mail.ru>
2025-01-29 14:13:38 +0400

    feat:add API route structure and fetch request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import styles from "./page.module.css";
 
-export default function Home() {
+export default async function Home() {
+    const games = await fetch(process.env.url + "/api/games").then((response)=>
+        response.json()
+    )
+    console.log(games)
+
     return (
         <div className={styles.page}>
-            <div className="fs3plus">
+            <div className={`${styles.title} fs3plus`}>
                 Наши игры
+            </div>
+            <div>
+                Захватывающие игры для всех возрастов,
+                которые позволяют погрузиться в мир виртуальной реальности и стать его частью.
             </div>
         </div>
     );
---
+++ src/interfaces/index.ts
@@ -0,0 +1,18 @@
+export interface Game {
+    id: number,
+    img: string,
+    type:'PVP' | 'PVE',
+    title: string;
+    description: string;
+    players: {
+        min: number;
+        max: number;
+    };
+    time: number;
+    age: number;
+    genre: string;
+}
+
+// export interface GameData {
+//     [key: number]: Game;
+// }
